ce4909297c2cc676b8f18ecc0fa13f86011bde8b
Anurag251 <rana.anurag12345@.com>
2022-08-16 13:15:52 +0545

    update

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
-export const urls = "http://192.168.1.118:8000/api/";
+// export const urls = "http://192.168.1.118:8000/api/";
+export const urls = "https://api.eliteslogistics.com/api/";
 // export const urls = "http://api.eliteslogistics.com/api/";
